0ec0ff806bfca4842f15bb58117b80eaa1460ecc
hrvp <hrvp2017@gmail.com>
2020-06-05 08:20:51 -0500

    Fin sección 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { InfoPaginaService } from './services/info-pagina.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portafolio';
+  constructor( public infoPaginaService: InfoPaginaService ) {
+
+  }
 }
