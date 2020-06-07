e716b104b776ad8c14bcadf6168e310edd121a50
hrvp <hrvp2017@gmail.com>
2020-06-07 18:10:43 -0500

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,13 +8,28 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info: InfoPagina = {};
   cargado = false;
+  equipo: any[] = [];
+
   constructor(private http: HttpClient ) {
+    //this.cargarInfo();
+    this.cargarEquipo();
+ }
+ private cargarInfo(){
         // console.log('Servicio de infoPagina listo');
-     this.http.get('assets/data/data-pagina.json')
-    .subscribe( (resp: InfoPagina) => {
-      this.cargado = true;
-      this.info = resp;
-      console.log(resp);
-    });
+        this.http.get('assets/data/data-pagina.json')
+        .subscribe( (resp: InfoPagina) => {
+          this.cargado = true;
+          this.info = resp;
+          console.log(resp);
+        });
+ }
+ private cargarEquipo(){
+  this.http.get('https://angular-html-8b53c.firebaseio.com/equipo.json')
+  .subscribe( (resp: any[]) => {
+    this.cargado = true;
+    this.equipo = resp;
+    //console.log(resp);
+  });
+
  }
 }
